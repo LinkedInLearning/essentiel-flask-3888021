9b4b34cddbef28c789a8d693051b12f236bcf44a
Labasse <labasse@gmail.com>
2024-05-27 12:24:09 +0000

    Répéter un motif (début)

--- app/portfolio.py
+++ app/portfolio.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from app.modeles import projets
+from app.modeles import projets, avis
 
 app = Flask(__name__)
 
@@ -7,3 +7,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html', liste=projets)
+
+
+@app.route("/projet")
+def projet():
+    return render_template('projet.html', projet=projets[0], avis=avis)
