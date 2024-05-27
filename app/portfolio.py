db9ed23c638202947ab685978e191cd600562d6a
Labasse <labasse@gmail.com>
2024-05-27 12:07:11 +0000

    Afficher les données (fin)

--- app/portfolio.py
+++ app/portfolio.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
+from app.modeles import projets
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', liste=projets)
