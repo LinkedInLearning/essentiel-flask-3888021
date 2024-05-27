d8e1a1706244a4540c3dbac136083bde61b9e960
Labasse <labasse@gmail.com>
2024-05-27 17:08:16 +0000

    Traiter les erreurs (fin)

--- app/portfolio.py
+++ app/portfolio.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from app.modeles import projets, avis
 
 app = Flask(__name__)
+
+
+@app.errorhandler(404)
+@app.route("/oups")
+def introuvable(e=None):
+    return render_template('introuvable.html')
 
 
 @app.route("/")
@@ -11,5 +17,7 @@ def index():
 
 @app.route("/projet/<int:idproj>")
 def projet(idproj):
-    projet = next(p for p in projets if p['id'] == idproj)
+    projet = next((p for p in projets if p['id'] == idproj), None)
+    if projet is None:
+        return redirect(url_for('introuvable'))
     return render_template('projet.html', projet=projet, avis=avis)
