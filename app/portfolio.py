9e67ffeb6ea0949afcd1161cb248c329fa4a0ac2
Labasse <labasse@gmail.com>
2024-05-27 14:16:53 +0000

    Changer de page (fin)

--- app/portfolio.py
+++ app/portfolio.py
@@ -9,6 +9,7 @@ def index():
     return render_template('index.html', liste=projets)
 
 
-@app.route("/projet")
-def projet():
-    return render_template('projet.html', projet=projets[1], avis=avis)
+@app.route("/projet/<int:idproj>")
+def projet(idproj):
+    projet = next(p for p in projets if p['id'] == idproj)
+    return render_template('projet.html', projet=projet, avis=avis)
