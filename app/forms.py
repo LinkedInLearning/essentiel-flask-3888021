2b0194622e63db89b82c589cb7baad28dd9e3255
Labasse <labasse@gmail.com>
2024-05-28 17:28:41 +0000

    Valider les saisies (début)

--- app/forms.py
+++ app/forms.py
@@ -8,7 +8,7 @@ class FormAvis(Form):
         csrf = True
         csrf_class = SessionCSRF
         csrf_secret = b'zYxKxs39RsZ2Xs7EvPXuU6AfON4'
-        csrf_time_limit = timedelta(seconds=10)
+        csrf_time_limit = timedelta(minutes=10)
 
     auteur = StringField('Nom (entreprise)')
     contenu = TextAreaField('Votre avis')
