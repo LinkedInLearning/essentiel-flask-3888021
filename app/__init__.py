1161e902772da1636614c3d9ae2022440026855d
Labasse <labasse@gmail.com>
2024-05-29 18:38:02 +0000

    Authentifier l'internaute (fin)

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, session
-from flask_security import Security, SQLAlchemyUserDatastore
+from flask_security import Security, SQLAlchemyUserDatastore, hash_password
 from flask_babel import Babel
 from app.modeles import db, Utilisateur, Role
 from os import path
@@ -18,6 +18,14 @@ def create_app():
     app.security = Security(
         app, SQLAlchemyUserDatastore(db, Utilisateur, Role)
     )
+
+    with app.app_context():
+        admin_mail = app.config['ADMIN_MAIL']
+        if not app.security.datastore.find_user(email=admin_mail):
+            app.security.datastore.create_user(
+                email=admin_mail,
+                password=hash_password(app.config['ADMIN_PASSE_INITIAL']))
+            db.session.commit()
 
     @app.errorhandler(404)
     @app.route("/oups")
