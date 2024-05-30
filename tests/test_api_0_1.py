40a4255c4c95650cecfad0e6d01f83c440a901e7
Labasse <labasse@gmail.com>
2024-05-30 20:15:53 +0000

    Implémenter les tests (début)

---
+++ tests/test_api_0_1.py
@@ -0,0 +1,28 @@
+def test_projet_avis_get_inexistant(client):
+  reponse = client.get('/v0.1/projets/123/avis')
+  assert 404 == reponse.status_code
+
+
+def test_projet_avis_get_existant(client):
+  reponse = client.get('/v0.1/projets/1/avis')
+  assert 200 == reponse.status_code
+
+
+def test_contacts_get_aucun_token(client):
+  reponse = client.get('/v0.1/contacts')
+  assert 401 == reponse.status_code
+
+
+def test_contacts_get_mauvais_token(client):
+  reponse = client.get('/v0.1/contacts')
+  assert 422 == reponse.status_code
+
+
+def test_contacts_get_token_client(client):
+  reponse = client.get('/v0.1/contacts')
+  assert 403 == reponse.status_code
+
+
+def test_contacts_get_token_admin(client):
+  reponse = client.get('/v0.1/contacts')
+  assert 200 == reponse.status_code
