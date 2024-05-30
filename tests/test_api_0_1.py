3203201ae32a476097dea3f5a0aebe3a99dd8987
Labasse <labasse@gmail.com>
2024-05-30 21:01:33 +0000

    Améliorer les performances (début)

--- tests/test_api_0_1.py
+++ tests/test_api_0_1.py
@@ -1,3 +1,6 @@
+from flask_jwt_extended import create_access_token
+
+
 def test_projet_avis_get_inexistant(client):
   reponse = client.get('/v0.1/projets/123/avis')
   assert 404 == reponse.status_code
@@ -6,6 +9,7 @@ def test_projet_avis_get_inexistant(client):
 def test_projet_avis_get_existant(client):
   reponse = client.get('/v0.1/projets/1/avis')
   assert 200 == reponse.status_code
+  assert 3 == len(reponse.json)
 
 
 def test_contacts_get_aucun_token(client):
@@ -14,15 +18,31 @@ def test_contacts_get_aucun_token(client):
 
 
 def test_contacts_get_mauvais_token(client):
-  reponse = client.get('/v0.1/contacts')
+  reponse = client.get('/v0.1/contacts', headers={
+    'Authorization': 'Bearer xyz'
+  })
   assert 422 == reponse.status_code
 
 
-def test_contacts_get_token_client(client):
-  reponse = client.get('/v0.1/contacts')
+def test_contacts_get_token_client(app, client):
+  with app.app_context():
+    jeton = create_access_token(identity={
+        'email': 'xxx',
+        'roles': ['client']
+    })
+  reponse = client.get('/v0.1/contacts', headers={
+    'Authorization': f'Bearer {jeton}'
+  })
   assert 403 == reponse.status_code
 
 
-def test_contacts_get_token_admin(client):
-  reponse = client.get('/v0.1/contacts')
+def test_contacts_get_token_admin(app, client):
+  with app.app_context():
+    jeton = create_access_token(identity={
+        'email': 'xxx',
+        'roles': ['admin']
+    })
+  reponse = client.get('/v0.1/contacts', headers={
+    'Authorization': f'Bearer {jeton}'
+  })
   assert 200 == reponse.status_code
