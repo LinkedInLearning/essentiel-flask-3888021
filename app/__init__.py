51b1a02777405597aeb7951fe042b226fbe3fec0
Labasse <labasse@gmail.com>
2024-05-30 19:56:25 +0000

    Garantir la qualité (fin)

--- app/__init__.py
+++ app/__init__.py
@@ -2,18 +2,20 @@ from flask import Flask, render_template, request, session
 from flask_security import Security, SQLAlchemyUserDatastore, hash_password
 from flask_babel import Babel
 from flask_jwt_extended import JWTManager
-from app.modeles import db, Utilisateur, Role
+from app.modeles import db, Utilisateur, Role, Projet, projets, Avis, avis
 from os import path
 from app import admin, client, portfolio, api_0_1
 from flask.cli import with_appcontext
 import click
 
 
-def create_app():
+def create_app(conf = None):
     app = Flask(__name__, 
                 instance_path=path.abspath('instance'), 
                 instance_relative_config=True)
     app.config.from_pyfile('config.py')
+    if conf:
+        app.config.update(conf)
     app.logger.setLevel(app.config['PORTFOLIO_NIVEAU_LOG'])
     db.init_app(app)
 
@@ -26,6 +28,11 @@ def create_app():
     app.logger.info('Sécurité ok')
 
     with app.app_context():
+        if app.config['TESTING']:
+            db.create_all()
+            db.session.add_all([Projet(**p) for p in projets])
+            db.session.add_all([Avis(**(a|{'ok':True})) for a in avis])
+            db.session.commit()
         app.security.datastore.find_or_create_role(name="admin")
         app.security.datastore.find_or_create_role(name="client")
         db.session.commit()
