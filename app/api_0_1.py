ad0904f43ac80828dee3c42c01e1062808295ab5
Labasse <labasse@gmail.com>
2024-05-30 08:29:18 +0000

    Sérialiser en JSON (fin)

--- app/api_0_1.py
+++ app/api_0_1.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from app.modeles import db, Projet
 
 bp = Blueprint('api_0_1', __name__, url_prefix='/v0.1')
@@ -9,12 +9,10 @@ bp = Blueprint('api_0_1', __name__, url_prefix='/v0.1')
 @bp.errorhandler(403)
 @bp.errorhandler(404)
 def erreur(e):
-    return { 'error': str(e) }, e.code
+    return jsonify(error=str(e)), e.code
 
 
 @bp.get('/projets/<int:idprojet>/avis')
 def projets_avis_get(idprojet):
   projet = db.get_or_404(Projet, idprojet)
-  return [{ 
-    'auteur': a.auteur, 'contenu': a.contenu, 'likes': a.likes 
-  } for a in projet.avis if a.ok]
+  return [a.dto() for a in projet.avis if a.ok]
